eff1f0393d74fa720bd1dbddf6bf68cbd9c6a195
livglm <maxmauro@solarisnet.it>
2016-02-19 16:36:34 +0100

    commit

--- __init__.py
+++ __init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import last_price
+from . import last_price
